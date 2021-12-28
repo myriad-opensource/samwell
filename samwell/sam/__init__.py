a0b9d4505a94d5dbb287669766ec22e4cd3fd841
Nathan Roach <nroach@myriad.com>
2021-12-28 13:59:31 -0500

    Fixing lints on newer mypy

--- samwell/sam/__init__.py
+++ samwell/sam/__init__.py
@@ -229,7 +229,7 @@ def _pysam_open(path: SamPath,
         raise TypeError(f"Cannot open '{type(path)}' for {open_type}.")
 
     if file_type is None and not open_for_reading:
-        raise ValueError(f"file_type must be given when writing to a file-like object")
+        raise ValueError("file_type must be given when writing to a file-like object")
 
     # file_type must be set when writing, so if file_type is None, then we must be opening it
     # for reading.  Hence, only set mode in kwargs to pysam when file_type is set and when
@@ -560,7 +560,7 @@ def set_pair_info(r1: AlignedSegment, r2: AlignedSegment, proper_pair: bool = Tr
     """
     assert not r1.is_unmapped, f"Cannot process unmapped mate {r1.query_name}/1"
     assert not r2.is_unmapped, f"Cannot process unmapped mate {r2.query_name}/2"
-    assert r1.query_name == r2.query_name, f"Attempting to pair reads with different qnames."
+    assert r1.query_name == r2.query_name, "Attempting to pair reads with different qnames."
 
     for r in [r1, r2]:
         r.is_paired = True
